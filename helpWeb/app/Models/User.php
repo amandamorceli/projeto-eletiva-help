5890ee1217374d7f2d68c8fb61d07091921d0d4c
Amanda Morceli Rodrigues <amandamorceli@gmail.com>
2025-03-30 22:02:56 -0300

    Criação das models e migrations no bd

--- helpWeb/app/Models/User.php
+++ helpWeb/app/Models/User.php
@@ -18,9 +18,23 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'c_login',
+        'c_senha',
+        'c_nome_completo',
+        'c_nome_resumido',
+        'c_cpf_cnpj',
+        'c_rg',
+        'f_tipo_fj',
+        'n_cep',
+        'c_endereco',
+        'c_numero_endereco',
+        'c_bairo',
+        'c_cidade',
+        'c_estado',
+        'd_inicio',
+        'd_fim',
+        'n_cod_usuario_inc',
+        'd_inclusao'
     ];
 
     /**
@@ -29,7 +43,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'c_senha',
         'remember_token',
     ];
 
@@ -39,7 +53,9 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'd_inicio' => 'datetime',
+        'd_fim' => 'datetime',
+        'd_inclusao'=>'datetime',
+        'c_senha' => 'hashed',
     ];
 }
