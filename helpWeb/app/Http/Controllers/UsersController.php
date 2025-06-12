33fbb86ad5d23fe03cd1c7bd003b1e4951dc47d1
amandamorceli <amandamorceli@gmail.com>
2025-06-12 09:02:23 -0300

    ajustes na apresentação

--- helpWeb/app/Http/Controllers/UsersController.php
+++ helpWeb/app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
